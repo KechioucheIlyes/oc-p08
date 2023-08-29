e87ab894e2fe65213541470740c2f81a022b0026
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-08-29 21:56:23 +0200

    backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@ const app = require("./app")
 
 const server = http.createServer(app)
 
-server.listen(process.env.PORT || 3333, () => {
+server.listen(process.env.PORT || 5001, () => {
     console.log("reussis")
 })
