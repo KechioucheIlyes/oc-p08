6b1d2ccc790799d22a612f9383ce214d5cf0cf9b
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-09-06 15:25:52 +0200

    ok

--- frontend/front/src/app/page.js
+++ frontend/front/src/app/page.js
@@ -14,7 +14,15 @@ export default function Home() {
   return (
     <>
 
+      
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-MHJ9GBEJY6"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments)}
+        gtag('js', new Date());
 
+        gtag('config', 'G-MHJ9GBEJY6');
+      </script>
 
       <Navbar />
 
