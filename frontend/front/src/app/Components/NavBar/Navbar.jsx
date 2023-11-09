0f423c032dd3ef211c5959a9f01fff158b61a64f
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-09 22:17:37 +0000

    now

--- frontend/front/src/app/Components/NavBar/Navbar.jsx
+++ frontend/front/src/app/Components/NavBar/Navbar.jsx
@@ -9,22 +9,39 @@ import { useEffect, useState } from 'react';
 
 
 const Navbar = () => {
-    
+    const [scrolling, setScrolling] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 0) {
+            setScrolling(true);
+        } else {
+            setScrolling(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+
     return (
-        <header className={`${styles.header}}`}>
-            
+        <header className={`${styles.header} ${scrolling ? styles.scrolling : ''}`}>
+
             <label className={styles.toggle} >
                 <input className={styles.checkbox} />
                 <span className={styles.slider} ></span>
             </label>
             <nav className={styles.navbar} >
                 <ul>
-                    <li><Link className='link'  href={"#about"}>À propos</Link></li>
-                    <li><Link className='link'  href={"#services"}>Services</Link></li>
-                    <li><Link className='link'  href={"#formation"}>Formation</Link></li>
-                    <li><Link className='link'  href={"#github"}>API Github</Link></li>
-                    <li><Link className='link'  href={"#competences"}>Competences</Link></li>
-                    <li><Link className='link'  href={"#contact"}>Contact</Link></li>
+                    <li><Link className='link' href={"#about"}>À propos</Link></li>
+                    <li><Link className='link' href={"#services"}>Services</Link></li>
+                    <li><Link className='link' href={"#formation"}>Formation</Link></li>
+                    <li><Link className='link' href={"#github"}>API Github</Link></li>
+                    <li><Link className='link' href={"#competences"}>Competences</Link></li>
+                    <li><Link className='link' href={"#contact"}>Contact</Link></li>
 
                 </ul>
 
