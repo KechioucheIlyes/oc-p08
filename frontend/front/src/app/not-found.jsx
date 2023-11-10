f1d00a229be246a5d55495a97ed262eeec807c71
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-10 09:46:46 +0000

    not found

--- frontend/front/src/app/not-found.jsx
+++ frontend/front/src/app/not-found.jsx
@@ -2,9 +2,23 @@
 
 
 export default function Notfound(){
-    return (
+    const images = [
+        'block1.jpg',
+        'block2.jpg',
+        'block3.jpg',
+        'block4.jpg',
+        // ... ajoutez autant d'images que nécessaire
+      ];
+    
+      return (
         <div>
-            not found
+          <h1>404 - Not Found</h1>
+          <p>Sorry, the page you are looking for does not exist.</p>
+          <div className="number-4">
+            {images.map((image, index) => (
+              <img key={index} src={image} alt={`Block ${index + 1}`} />
+            ))}
+          </div>
         </div>
-    )
+      );
 }
