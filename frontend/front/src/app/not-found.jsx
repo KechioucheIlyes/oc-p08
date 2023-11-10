45ba56c289b94a6906e74da0fa56415a0d3d8711
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-10 14:07:44 +0000

    validation

--- frontend/front/src/app/not-found.jsx
+++ frontend/front/src/app/not-found.jsx
@@ -35,12 +35,7 @@ export default function Notfound(){
               {images.map((image, index) => (
                 <Image className='images' key={index} src={image} width={90} height={70} alt={`Block ${index + 1}`} />
               ))}
-              <div class="number4">
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bar"></div>
-                <div class="bottom"></div>
-              </div>
+              
             </div>
             <div className="number-4">
               {images.map((image, index) => (
