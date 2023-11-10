4b35dcfad011d970624ed2670fa0e9cb6d47ba03
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-10 15:37:45 +0000

    ok

--- frontend/front/src/app/not-found.jsx
+++ frontend/front/src/app/not-found.jsx
@@ -1,6 +1,15 @@
-'use client'
-import React from 'react';
 
+'use client'
+import Image from 'next/image'
+import scrapPic from "./assets/Mail_scrap.png"
+import bookiPic from "./assets/Booki.png"
+import carducciPic from "./assets/carducci.png"
+import MVG from "./assets/P07.png"
+import Kasa from "./assets/ScreenshotKasa.png"
+import SohpieProject from "./assets/P03.png"
+import ViteTodo from "./assets/todoList.png"
+import defaultPic from "./assets/defaultpic.jpg"
+import CirclePic from './Components/CirclePic/CirclePic'
 
 
 export default function Notfound() {
