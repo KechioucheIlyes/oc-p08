cdadb41c3ef74b5bac4af56e592170535683a442
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-10 15:36:34 +0000

    test

--- frontend/front/src/app/not-found.jsx
+++ frontend/front/src/app/not-found.jsx
@@ -1,42 +1,47 @@
 'use client'
-import Image from 'next/image'
-import scrapPic from "./assets/Mail_scrap.png"
-import bookiPic from "./assets/Booki.png"
-import carducciPic from "./assets/carducci.png"
-import MVG from "./assets/P07.png"
-import Kasa from "./assets/ScreenshotKasa.png"
-import SohpieProject from "./assets/P03.png"
-import ViteTodo from "./assets/todoList.png"
-import defaultPic from "./assets/defaultpic.jpg"
-import CirclePic from './Components/CirclePic/CirclePic'
+import React from 'react';
 
 
-export default function Notfound(){
-    const images = [
-        scrapPic    ,
-        bookiPic,
-        carducciPic,
-        MVG,
-        Kasa,
-        SohpieProject,
-        ViteTodo,
-        defaultPic,
-      ];
 
-      
+export default function Notfound() {
+  const images = [
+    scrapPic,
+    bookiPic,
+    carducciPic,
+    MVG,
+    Kasa,
+    SohpieProject,
+    ViteTodo,
+    defaultPic,
+  ];
 
-      return (
-        <>
-          <div id='circle-container' className='not-found-wrapper'>
-              
-              { images.map((element) => {
-                  return <CirclePic/>
-              }) }
+  const circleRadius = 100; 
+  const centerCoordinates = { x: 200, y: 200 }; 
 
-          </div>
-          <p>Vous avez attérie dans une page qui n'existe pas vous avez tout de meme le choix entre consulter mes projet Git ou de revenir a l'acceuil !</p>
-            
+  return (
+    <>
+      <div id='circle-container' className='not-found-wrapper'>
+        {images.map((element, index) => {
+          // Calculez les coordonnées du cercle
+          const theta = (index / images.length) * 2 * Math.PI;
+          const x = centerCoordinates.x + circleRadius * Math.cos(theta);
+          const y = centerCoordinates.y + circleRadius * Math.sin(theta);
+
           
-        </>
-      );
+          const circleStyle = {
+            position: 'absolute',
+            left: `${x}px`,
+            top: `${y}px`,
+            transform: 'translate(-50%, -50%)', 
+            borderRadius: '50%', 
+          };
+
+          return <CirclePic key={index} style={circleStyle} />;
+        })}
+      </div>
+      <p>
+        Vous avez atterri sur une page qui n'existe pas. Vous avez tout de même le choix entre consulter mes projets Git ou revenir à l'accueil !
+      </p>
+    </>
+  );
 }
