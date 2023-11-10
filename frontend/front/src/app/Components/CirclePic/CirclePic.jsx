4fd24a6fb5b3d44f995e326bdc4c5031441e920c
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-10 15:14:24 +0000

    ok

---
+++ frontend/front/src/app/Components/CirclePic/CirclePic.jsx
@@ -0,0 +1,14 @@
+
+export default function CirclePic({}){
+
+
+    return (
+        <div id="circle-container">
+            
+            <div className="circle"></div>
+        </div>
+    )
+
+
+
+}
