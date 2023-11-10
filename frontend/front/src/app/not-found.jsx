a11ed98b84f9cb331b2a7baddc0f6f1723cec3ab
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-10 10:00:02 +0000

    adding width and height

--- frontend/front/src/app/not-found.jsx
+++ frontend/front/src/app/not-found.jsx
@@ -21,7 +21,6 @@ export default function Notfound(){
         ViteTodo,
         defaultPic,
         
-        // ... ajoutez autant d'images que nécessaire
       ];
     
       return (
@@ -30,7 +29,7 @@ export default function Notfound(){
           <p>Sorry, the page you are looking for does not exist.</p>
           <div className="number-4">
             {images.map((image, index) => (
-              <Image key={index} src={image} alt={`Block ${index + 1}`} />
+              <Image key={index} src={image} width={200} height={200} alt={`Block ${index + 1}`} />
             ))}
           </div>
         </div>
