f8d22fc8f8ac65d399a2162757a1594d3c2eda16
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-10 15:43:09 +0000

    test

--- frontend/front/src/app/Components/CirclePic/CirclePic.jsx
+++ frontend/front/src/app/Components/CirclePic/CirclePic.jsx
@@ -1,15 +1,29 @@
 
-export default function CirclePic({}){
-
-    
-    
+export default function CirclePic({ image, index, totalImages }) {
+    const circleRadius = 100; // Ajustez le rayon selon vos besoins
+    const centerCoordinates = { x: 200, y: 200 }; // Ajustez les coordonnées du centre selon vos besoins
+  
+    // Calculez les coordonnées du cercle
+    const theta = (index / totalImages) * 2 * Math.PI;
+    const x = centerCoordinates.x + circleRadius * Math.cos(theta);
+    const y = centerCoordinates.y + circleRadius * Math.sin(theta);
+  
+    // Appliquez les styles pour positionner l'image en tant que cercle
+    const circleStyle = {
+      position: 'absolute',
+      left: `${x}px`,
+      top: `${y}px`,
+      transform: 'translate(-50%, -50%)', // Centrez l'image sur les coordonnées calculées
+      borderRadius: '50%', // Rendez l'image circulaire
+      backgroundImage: `url(${image})`, // Utilisez l'image passée en tant que background
+      backgroundSize: 'cover', // Ajustez la taille de l'image
+      width: '50px', // Ajustez la largeur de l'image selon vos besoins
+      height: '50px', // Ajustez la hauteur de l'image selon vos besoins
+    };
+  
     return (
-        
-            
-            <div className="circle"></div>
-       
-    )
-
-
-
-}
+      <div className="circle" style={circleStyle}>
+        {/* Vous pouvez également ajouter du contenu à l'intérieur du cercle si nécessaire */}
+      </div>
+    );
+  }
--- frontend/front/src/app/not-found.jsx
+++ frontend/front/src/app/not-found.jsx
@@ -24,29 +24,12 @@ export default function Notfound() {
     defaultPic,
   ];
 
-  const circleRadius = 100; 
-  const centerCoordinates = { x: 200, y: 200 }; 
-
   return (
     <>
       <div id='circle-container' className='not-found-wrapper'>
-        {images.map((element, index) => {
-          // Calculez les coordonnées du cercle
-          const theta = (index / images.length) * 2 * Math.PI;
-          const x = centerCoordinates.x + circleRadius * Math.cos(theta);
-          const y = centerCoordinates.y + circleRadius * Math.sin(theta);
-
-          
-          const circleStyle = {
-            position: 'absolute',
-            left: `${x}px`,
-            top: `${y}px`,
-            transform: 'translate(-50%, -50%)', 
-            borderRadius: '50%', 
-          };
-
-          return <CirclePic key={index} style={circleStyle} />;
-        })}
+        {images.map((element, index) => (
+          <CirclePic key={index} image={element} index={index} totalImages={images.length} />
+        ))}
       </div>
       <p>
         Vous avez atterri sur une page qui n'existe pas. Vous avez tout de même le choix entre consulter mes projets Git ou revenir à l'accueil !
@@ -54,3 +37,9 @@ export default function Notfound() {
     </>
   );
 }
+
+
+
+
+
+
