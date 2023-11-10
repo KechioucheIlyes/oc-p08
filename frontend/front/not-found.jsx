7cafe375ca3e408b7da4eaaa2c9b5b5c6ede1e3a
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-10 09:45:09 +0000

    deplacement not-found

--- frontend/front/not-found.jsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-
-export default function NotFound(){
-
-    return (
-        <div>Not Found</div>
-    )
-} 
---
+++ frontend/front/src/app/not-found.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+
+export default function Notfound(){
+    return (
+        <div>
+            not found
+        </div>
+    )
+}
