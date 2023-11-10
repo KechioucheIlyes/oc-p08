98933f6b6b081dfdecdf98930a4d2f90be42ddc4
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2023-11-10 15:59:45 +0000

    ok

--- frontend/front/src/app/Components/CirclePic/CirclePic.jsx
+++ frontend/front/src/app/Components/CirclePic/CirclePic.jsx
@@ -1,32 +1,37 @@
-
 export default function CirclePic({ image, index, totalImages }) {
     const circleRadius = 100; // Ajustez le rayon selon vos besoins
     const centerCoordinates = { x: 200, y: 200 }; // Ajustez les coordonnées du centre selon vos besoins
   
-    // Calculez les coordonnées du cercle
+    
     const theta = (index / totalImages) * 2 * Math.PI;
     const x = centerCoordinates.x + circleRadius * Math.cos(theta);
     const y = centerCoordinates.y + circleRadius * Math.sin(theta);
   
-    // Appliquez les styles pour positionner l'image en tant que cercle
+    
     const circleStyle = {
       position: 'absolute',
       left: `${x}px`,
       top: `${y}px`,
-      transform: 'translate(-50%, -50%)', // Centrez l'image sur les coordonnées calculées
-      borderRadius: '50%', // Rendez l'image circulaire
-      backgroundImage: `url(${image})`, // Utilisez l'image passée en tant que background
-      backgroundSize: 'cover', // Ajustez la taille de l'image
-      width: '50px', // Ajustez la largeur de l'image selon vos besoins
-      height: '50px', // Ajustez la hauteur de l'image selon vos besoins
+      transform: 'translate(-50%, -50%)', 
+      borderRadius: '50%', 
+      backgroundImage: `url(${image})`, 
+      backgroundSize: 'cover', 
+      width: '50px', 
+      height: '50px', 
+    };
+  
+    // Appliquez les styles pour le conteneur Flexbox
+    const containerStyle = {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh', 
     };
   
     return (
-        <div className="circle-contain">
-            <div className="circle" style={circleStyle}>
-
-                </div>
-        </div>
-      
+      <div style={containerStyle}>
+        <div className="circle" style={circleStyle}></div>
+      </div>
     );
   }
+  
