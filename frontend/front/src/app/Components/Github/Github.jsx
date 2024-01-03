dc96d030881ed839563c5c4f2845cd30f5441d93
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2024-01-03 18:09:05 +0000

    ok

--- frontend/front/src/app/Components/Github/Github.jsx
+++ frontend/front/src/app/Components/Github/Github.jsx
@@ -189,8 +189,22 @@ const Github = () => {
                                         </div> 
                                     </Typography>
                                     <Typography id="modal-desc" textColor="text.tertiary">
+                                        <div className={styles.modalContainer}>
                                          <code>aria-labelledby</code> on the modal dialog with an
                                         optional <code>aria-describedby</code> attribute.
+                                        <div className={styles.modalPic} >
+                                            {infos ? infos.pics_url.map(pics => {
+                                                return (
+                                                    <Image src={pics} alt='pics' height={100} width={100} priority />
+                                                )
+                                            }) : null}
+                                        </div>
+
+                                        <div className={styles.modalText}>
+
+                                        </div>
+
+                                        </div>
                                     </Typography>
                                     </Sheet>
                                 </Modal>
