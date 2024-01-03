be4084ccae8e087322a77d0e95a7178892b56a9f
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2024-01-03 15:12:18 +0000

    test

--- frontend/front/src/app/Components/Github/Github.jsx
+++ frontend/front/src/app/Components/Github/Github.jsx
@@ -65,6 +65,7 @@ const Github = () => {
 
                         {fichierGit.map((info, index) => {
                             return (<li className={styles.githubItem} key={info.id}>
+                                <div  className={styles.card}>
                                     <div className={styles.cardHeader}>
                                         {info.name === "BOOKI" ? (<Image className={styles.imageCard} src={bookiPic} width={350} height={200} priority alt={`${info.name}-image`} />) :
                                             info.name === "Mail-Scrap-Mini-project" ? (<Image className={styles.imageCard} src={ScrapPic} width={350} height={200} priority alt={`${info.name}-image`} />) :
@@ -137,7 +138,7 @@ const Github = () => {
                                     </div>
 
 
-                                
+                                </div>
                             </li>)
                         })}
                     </ul>
