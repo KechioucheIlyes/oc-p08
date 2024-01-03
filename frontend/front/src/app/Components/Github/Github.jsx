5db769241d834096234b615d8e93aa3298dbb009
KechioucheIlyes <112709633+KechioucheIlyes@users.noreply.github.com>
2024-01-03 18:34:10 +0000

    ok

--- frontend/front/src/app/Components/Github/Github.jsx
+++ frontend/front/src/app/Components/Github/Github.jsx
@@ -193,7 +193,7 @@ const Github = () => {
                                          <code>aria-labelledby</code> on the modal dialog with an
                                         optional <code>aria-describedby</code> attribute.
                                         <div className={styles.modalPic} >
-                                            {infos.pics_url}
+                                            {infos ?  infos.pics_url : null}
                                         </div>
 
                                         <div className={styles.modalText}>
